7c5bd79ab5d64c3b6b8d754fdca95d67af819345
Benny Chan <benny_chan@asus.com>
2025-03-12 16:01:47 +0800

    Update reranker
    Add top_n in constructor

--- reranker.py
+++ reranker.py
@@ -4,21 +4,21 @@ import utils
 
 
 class Reranker:
-    def __init__(self):
+    def __init__(self, top_n: int = None):
         self.reranker = FlagReranker(utils.get_reranker_path(), use_fp16=True, local_files_only=True)
+        self.top_n = top_n
 
     def __compute_reranking_score(self, query: str, passages: list[str]):
         return self.reranker.compute_score([(query, passage) for passage in passages], normalize=True)
 
-    def do_rerank(self, query: str, passages: list[str], count=None) -> list[str]:
+    def do_rerank(self, query: str, passages: list[str]) -> list[str]:
         """
         :param query: query given by user
         :param passages: relevant chunks given by embedding
-        :param count: number of results of reranking
-        :return: {count} most relevant result
+        :return: top_n most relevant result
         """
         scores = self.__compute_reranking_score(query, passages)
         result = sorted(zip(passages, scores), key=lambda x: x[1], reverse=True)
         # print(result)
-        return [r[0] for r in result[:count]]
-        # return result[:count]
+        return [r[0] for r in result[:self.top_n]]
+        # return result[:self.top_n]
